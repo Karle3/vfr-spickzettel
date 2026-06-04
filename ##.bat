3ef842306b0d1c0a289770b9ed20391e26bf137c
Karle3 <web@dentrich.de>
2026-06-04 12:39:25 +0200

    prepare v2_7: Boxes adjusted, auxilliary files added.

--- ##.bat
+++ ##.bat
@@ -1,6 +1,8 @@
 
+
+:: without .pylintrc,  local justifications
 pylint vfr_spickzettel_plot.py
 
-py -m pydoc -w vfr_spickzettel_plot
+:: UGLY: py -m pydoc -w vfr_spickzettel_plot
 
 start "run implicite PY"  vfr_spickzettel_plot.py
